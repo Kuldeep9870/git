cbb385aeef12167c545e2ad3e200e70b9a222912
kuldeep9870 <kuldeep26285@gmail.com>
2024-09-29 21:20:08 +0530

    feat 2

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ console.log('github')
 
 
 function newfeatureinbranch() {
-    console.log('THis is new feature added in branch');
-    
+    console.log('THis is new feature 1 added in branch ');
+    console.log('THis is new feature 2 added in branch ');
 }
